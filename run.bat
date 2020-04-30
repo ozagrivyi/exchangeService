79ed4f021dfc2cb94389a7ffe5098fc1e1837acd
ozagrivy <oleg.zagrivy@dataart.com>
2020-04-30 10:54:35 +0300

    Running of unit tests has been add to run script

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 echo Stop and Remove
 docker-compose -f docker-compose.yml down
 echo Build
-call gradlew.bat clean assemble
+call gradlew.bat clean test assemble
 echo Run
 docker-compose -f docker-compose.yml up --build --force-recreate -d
